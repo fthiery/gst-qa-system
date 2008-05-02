f1cf0cd285fac810624a2acfe2f1f5e278da3da7
Edward Hervey <bilboed@bilboed.com>
2008-05-02 14:08:07 +0200

    Convert test arguments to tuples, improve sqlite storage speed

--- gstqa/storage/sqlite.py
+++ gstqa/storage/sqlite.py
@@ -404,26 +404,20 @@ class SQLiteStorage(DBStorage):
             debug("Empty list, returning")
             return
 
-        # figure out which values to add to which tables
-        strs = []
-        ints = []
-        blobs = []
+        cur = self.con.cursor()
         insertstr = "INSERT INTO %s (id, containerid, name, %s) VALUES (NULL, ?, ?, ?)"
         for key,value in pdict:
             debug("Adding key:%s , value:%r", key, value)
             val = value
             if isinstance(value, int):
                 valstr = "intvalue"
-                lst = ints
             elif isinstance(value, basestring):
                 valstr = "txtvalue"
-                lst = strs
             else:
                 valstr = "blobvalue"
-                lst = blobs
                 val = sqlite.Binary(dumps(value))
             comstr = insertstr % (dicttable, valstr)
-            lst.append(self._ExecuteCommit(comstr, (containerid, key, val)))
+            cur.execute(comstr, (containerid, key, val))
 
     def _storeTestArgumentsDict(self, testid, dict):
         return self._storeDict("test_arguments_dict", testid, dict)
