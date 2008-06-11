e004a75a50dd47242f24e3c4ea8bae316b370363
Edward Hervey <bilboed@bilboed.com>
2008-06-11 19:11:19 +0200

    web : Add test view and templates and integrate it in the rest.

--- web/insanity/views.py
+++ web/insanity/views.py
@@ -1,4 +1,4 @@
-from web.insanity.models import TestRun
+from web.insanity.models import TestRun, Test
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 import time
@@ -10,6 +10,10 @@ def index(request):
 def testrun_summary(request, testrun_id):
     tr = get_object_or_404(TestRun, pk=testrun_id)
     return render_to_response('insanity/testrun_summary.html', {'testrun': tr})
+
+def test_summary(request, test_id):
+    tr = get_object_or_404(Test, pk=test_id)
+    return render_to_response('insanity/test_summary.html', {'test': tr})
 
 def handler404(request):
     return "Something went wrong !"
