7da93e41457a411acaed313f9785b9d2172202bb
Edward Hervey <bilboed@bilboed.com>
2008-09-16 16:55:56 +0200

    web : Have a separate directory for static files, use one .css for all views

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 from django import VERSION
+from django.conf import settings
+import os
 
 if VERSION >= (1, 0, ''):
     from django.contrib import admin
@@ -11,3 +13,13 @@ else:
     urlpatterns = patterns('',
                            (r'^insanity/', include('web.insanity.urls')),
                            (r'^admin/', include('django.contrib.admin.urls')))
+if settings.DEBUG:
+    # This is temporary
+    # DO NOT USE IN PRODUCTION. See django documentation about serving
+    # static files
+
+    # in the meantime... we take ../site_media/
+    docroot = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'site_media')
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': docroot}),
+    )
